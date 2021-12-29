59d8bcde84df1e32b30258c2062755ccc079d93c
DorobantuAndrei <dorobantuandrei19@stud.ase.ro>
2021-12-29 03:32:19 +0200

    added registration with google

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,14 +3,28 @@ import { Link } from 'react-router-dom';
 import './Login.css';
 import Axios from 'axios';
 import {useNavigate} from 'react-router-dom';
-
+import GoogleLogin from 'react-google-login';
 function Login(){
 
     const[usernameLogin,setUserName] = useState('');
     const[passwordLogin,setPassword] = useState('');
+
+    const[usernameRegister,setUserNameRegister] = useState('');
+    const[passwordRegister,setPasswordRegister] = useState('');
     const navigate = useNavigate();
     const handleSubmit=(e) => {
         e.preventDefault();
+    }
+
+   
+
+    function getRandomString(length) {
+        var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var result = '';
+        for ( var i = 0; i < length; i++ ) {
+            result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+        }
+        return result;
     }
 
    
@@ -35,6 +49,30 @@ function Login(){
         })
     }
 
+    const responseGoogle = response => {
+        console.log(response.profileObj);
+        setUserNameRegister(response.profileObj.name);
+        setPasswordRegister(getRandomString(10));
+        
+        
+
+        Axios.post('http://localhost:7000/api/postUser' , {
+            username: usernameRegister,
+            password: passwordRegister,
+            email:response.profileObj.email,
+            firstName:response.profileObj.name,
+            lastName:''
+        }).then((respone)=>{
+            console.log(respone);
+        })
+
+        alert(`Congratulations , you created an account . Your user name is : ${usernameRegister} , and your password is ${passwordRegister}`);
+
+        
+        
+
+    }
+
     
 
     return(
@@ -52,6 +90,17 @@ function Login(){
                     <div className='link-to-Register'>
                         <Link to='/Register' className='text-to-Register'>You don`t have an account ? Click here</Link>
                     </div>
+                    <div>
+                    <GoogleLogin
+                        clientId="491150127675-otr5lb46n99j50seb57diliajkplbhv4.apps.googleusercontent.com"
+                        buttonText="Register with google"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                     />
+                    </div>
+
+                    
                     
                 </form>
 
